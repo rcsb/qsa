49c2a31796f8adff1f46ae32b6641203f3b66c12
Antonin Pavelka <antonin.pavelka@gmail.com>
2016-08-31 15:30:26 -0700

    First commit

---
+++ src/main/java/org/rcsb/qsa/App.java
@@ -0,0 +1,13 @@
+package org.rcsb.qsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
