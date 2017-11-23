f2df09d5725bd8f610768bd05115f36c0f08ab7d
altaite <antonin.pavelka@gmail.com>
2017-11-22 20:41:28 -0800

    data

--- data/install.bat
+++ data/install.bat
@@ -1,4 +1,3 @@
 git clone https://github.com/rcsb/qsa.git
 cd qsa
 mvn install
-cd ..
--- data/run.sh
+++ data/run.sh
@@ -1,2 +1,2 @@
-SET MAVEN_OPTS=-Xmx12g
+MAVEN_OPTS=-Xmx12g
 mvn exec:java -Dexec.mainClass="analysis.Job" -Dexec.args="-h data"
